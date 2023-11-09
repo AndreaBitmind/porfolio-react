dc1e327358322fe4818014c3e7a011e9ee7926a7
Andrea Martinez <andrea.maort@gmail.com>
2023-11-09 14:56:21 +0100

    feat: added responsive design logic

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiGithub, SiGmail } from "react-icons/si";
 import { logo } from "../../assets/index";
 import { navLinksdata } from "../../constants";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
@@ -30,9 +34,47 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <span className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer">
+        <span
+          onClick={() => setShowMenu(!showMenu)}
+          className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
+        >
           <FiMenu />
         </span>
+        {showMenu && (
+          <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
+            <div className="flex flex-col gap-8 py-2 relative">
+              <div>
+                <img className="w-24" src={logo} alt="logo" />
+              </div>
+              <ul className="flex flex-col gap-4">
+                {navLinksdata.map((item) => (
+                  <li
+                    key={item._id}
+                    className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+                  >
+                    <Link
+                      onClick={() => setShowMenu(false)}
+                      activeClass="active"
+                      to={item.link}
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <span
+                onClick={() => setShowMenu(false)}
+                className="absolute top-4 right-4 text-gray-400 hover:text-designColor duration-300 text-2xl cursor-pointer"
+              >
+                <MdClose />
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
