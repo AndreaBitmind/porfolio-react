a305121b8fdb9ff53c36b34fdb1c1222b6704c98
Andrea Martinez <andrea.maort@gmail.com>
2023-11-15 13:22:40 +0100

    feat: app component added

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Banner from "./components/banner/Banner";
+import Features from "./components/features/Features";
+import FooterBottom from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Resume from "./components/resume/Resume";
+import Skills from "./components/skills/Skills";
+
+function App() {
+  return (
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+      <Navbar />
+      <div className="max-w-screen-xl mx-auto">
+        <Banner />
+        <Features />
+        <Skills />
+        <Projects />
+        <Resume />
+        <Contact />
+        <FooterBottom />
+      </div>
+    </div>
+  );
+}
+
+export default App;
