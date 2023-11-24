ab913460c299eb26d6b4685c7578b225410137f1
Andrea Martinez <andrea.maort@gmail.com>
2023-11-24 12:05:30 +0100

    style: changed project management icon and removed hover from mobile view

--- src/components/features/Card.js
+++ src/components/features/Card.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Card = ({ item: { title, des, icon } }) => {
   return (
-    <div className="w-full px-8 h-fit py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group">
-      <div className="h-80">
-        <div className="flex h-full flex-col gap-10 translate-y-16 group-hover:translate-y-0 transition-transform duration-500">
+    <div className="w-full px-8 h-fit py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group lgl:hover:bg-gradient-to-b lgl:hover:from-black lgl:hover:to-[#1e2024] lgl:transition-colors lgl:duration-100 group">
+      <div className="h-72 xl:h-96">
+        <div className="flex h-full flex-col gap-10 lgl:translate-y-16 lgl:group-hover:translate-y-0 lgl:transition-transform lgl:duration-500">
           <div className="w-10 h-8 flex flex-col justify-between">
             {icon ? (
               <span className="text-5xl text-designColor">{icon}</span>
